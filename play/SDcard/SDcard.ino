bf9a60db8300517e679dc7fa553ffed6a8c8a032
SatoshiNagura <rnrnrnrg@gmail.com>
2021-08-31 20:08:41 +0900

    追加

--- play/SDcard/SDcard.ino
+++ play/SDcard/SDcard.ino
@@ -5,18 +5,35 @@ Sd2Card card;
 SdVolume volume;
 SdFile root;
 const int cspin = 4;
-/*
+
+void serial_setting();//(1)
+void sd_setting();//(2)
+
 void setup(){
+  serial_setting();//(1)
+  sd_setting();//(2)
+}
+
+void loop(){}
+///////////////////////////////////////////////////////
+///////////////////////////////////////////////////////
+
+//(1) シリアル通信の初期設定
+void serial_setting(){
   Serial.begin(9600);
   while(!Serial);
   Serial.println("Serial Start.");
-  if(!card.init(SPI_HALF_SPEED, cspin)){
-    Serial.println("Failed.");
-  }
-  else{
-    Serial.println("Successed.");
-  }
-  Serial.println();
+}
+
+//(2) sdカードの初期設定
+void sd_setting(){
+  /*begin()関数を使った呼び出し
+  while(SD.begin(cspin));
+  Serial.println("Hello SD Card.");
+  */
+  //SDクラスの中のSd2Cardクラスを使った呼び出し
+  while(!card.init(SPI_HALF_SPEED, cspin));
+  Serial.println("Successed.");
   Serial.print("Card type: ");
   switch(card.type()){
     case SD_CARD_TYPE_SD1:
@@ -30,15 +47,4 @@ void setup(){
       break;
     default:
       Serial.println("Unknown.");
-  }
-}*/
-
-void setup(){
-  Serial.begin(9600);
-  while(!Serial);
-  Serial.println("Serial Start.");
-  while(SD.begin(cspin));
-  Serial.println("SD card.");
 }
-
-void loop(){}
