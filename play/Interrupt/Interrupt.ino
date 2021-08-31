419a20314f277808c4a29d1584a4c603707e0218
SatoshiNagura <rnrnrnrg@gmail.com>
2021-08-31 14:36:29 +0900

    時間割り込み

--- play/Interrupt/Interrupt.ino
+++ play/Interrupt/Interrupt.ino
@@ -1,22 +1,30 @@
 #include <DS3232RTC.h>
 
-//pin番号の定義
-#define SQW_pin 2
-
 //諸変数の定義
-volatile bool caughtAlarm = false;
-int alarmCout = 0;
+volatile bool caughtAlarm = false;//RTCからのアラーム検出
+int alarmCout = 0;//アラームの回数
 
 void serial_setting();//(1)
-void time_setting();//(2)
+void rtc_setting();//(2)
 
 void setup(){
   serial_setting();//(1)
-  time_setting();//(2)
+  rtc_setting();//(2)
+  //アラームを設定
+  RTC.setAlarm(ALM1_EVERY_SECOND, 0, 0, 0, 0);
+  RTC.alarm(ALARM_1);
+}
+time_t t;
+void loop(){
+  if(RTC.alarm(ALARM_1)){
+    t = RTC.get();
+    Serial.println(minute());
+  }
+  delay(100);
 }
 
-void loop(){
-}
+///////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////
 
 //シリアル通信の初期設定をする関数  (1)
 void serial_setting(){
@@ -26,9 +34,8 @@ void serial_setting(){
 }
 
 //RTCモジュールの初期設定をする関数  (2)
-void time_setting(){
-  setTime(14, 00, 00, 29, 8, 2021);//Arduinoに現在時刻を打ち込む
-  RTC.set(now());//RTCモジュールに時刻を同期させる
+void rtc_setting(){
+  RTC.set(compileTime());//RTCモジュールにコンパイル時刻を同期させる
   setSyncProvider(RTC.get);//RTCモジュールから時刻を取得
   if(timeStatus() != timeSet){//取得した時刻とarduinoの時刻が異なれば失敗を表示。
     Serial.println("×");
@@ -36,4 +43,35 @@ void time_setting(){
   else{
     Serial.println(now());
   }
+  //アラーム機能の初期無効化
+  RTC.setAlarm(ALM1_MATCH_DATE, 0, 0, 0, 1);
+  RTC.setAlarm(ALM2_MATCH_DATE, 0, 0, 0, 1);
+  RTC.alarm(ALARM_1);
+  RTC.alarm(ALARM_2);
+  RTC.alarmInterrupt(ALARM_1, false);//アラーム無効
+  RTC.alarmInterrupt(ALARM_2, false);
+  RTC.squareWave(SQWAVE_NONE);//pinへsqwの入力を無効
+}
+
+//コンパイルした時間を記憶する関数（./../alarm_ex.inoより引用）
+// function to return the compile date and time as a time_t value
+time_t compileTime(){
+    const time_t FUDGE(10);    //fudge factor to allow for upload time, etc. (seconds, YMMV)
+    const char *compDate = __DATE__, *compTime = __TIME__, *months = "JanFebMarAprMayJunJulAugSepOctNovDec";
+    char compMon[4], *m;
+
+    strncpy(compMon, compDate, 3);
+    compMon[3] = '\0';
+    m = strstr(months, compMon);
+
+    tmElements_t tm;
+    tm.Month = ((m - months) / 3 + 1);
+    tm.Day = atoi(compDate + 4);
+    tm.Year = atoi(compDate + 7) - 1970;
+    tm.Hour = atoi(compTime);
+    tm.Minute = atoi(compTime + 3);
+    tm.Second = atoi(compTime + 6);
+
+    time_t t = makeTime(tm);
+    return t + FUDGE;        //add fudge factor to allow for compile time
 }
